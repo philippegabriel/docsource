a3117afe48f99d1242606a25695fe333c308f131
philippe gabriel <philippe.gabriel@citrix.com>
2015-10-14 14:18:27 +0000

    fixed script for multiline

--- transform.pl
+++ transform.pl
@@ -1,23 +1,24 @@
 #!/usr/bin/perl
-while(<>){
+BEGIN {undef $/;}
+$input=<>;
 #Remove Headers:
-s/<div class="navheader">.+?<\/div>//smg;
+$input =~ s/<div class="navheader">.+?<\/div>//smg;
 #Remove Footers
-s/<div class="navfooter">.*?<\/div>//smg;
+$input =~ s/<div class="navfooter">.*?<\/div>//smg;
 #Remove Images from tables:
-s/<div class="mediaobject">.*?<\/div>//smg;
+$input =~ s/<div class="mediaobject">.*?<\/div>//smg;
 #Change ALL <code class> to <samp class="phcodeph">
-s/<code class="literal">(.*?)<\/code>/<samp class="ph codeph">\1<\/samp>/mg;
+$input =~ s/<code class="literal">(.*?)<\/code>/<samp class="ph codeph">\1<\/samp>/smg;
 #Remove <span class="emphasis"> & Change <em> to <i>
-s/<span class="emphasis"><em>(.*?)<\/em><\/span>/<i>\1<\/i>/mg;
+$input =~ s/<span class="emphasis"><em>(.*?)<\/em><\/span>/<i>\1<\/i>/smg;
 #Change <em class ="parameter"> to <i>
-s/<em class="parameter">(.*?)<\/em>/<i>\1<\/i>/mg;
+$input =~ s/<em class="parameter">(.*?)<\/em>/<i>\1<\/i>/smg;
 #Change <pre class="screen"> to <pre class ="code">
-s/<pre class="screen">(.*?)<\/pre>/<pre class="pre codeblock">\1<\/pre>/mg;
+$input =~ s/<pre class="screen">(.*?)<\/pre>/<pre class="pre codeblock">\1<\/pre>/smg;
 #Change <em class ="replaceable"> to <i>
-s/<em class="replaceable">(.*?)<\/em>/<i>\1<\/i>/mg;
+$input =~ s/<em class="replaceable">(.*?)<\/em>/<i>\1<\/i>/smg;
 #Change <span class="command"> to <b> & Remove <strong>
-s/<span class="command"><strong>(.*?)<\/strong><\/span>/<b>\1<\/b>/mg;
-print;
-}
+$input =~ s/<span class="command"><strong>(.*?)<\/strong><\/span>/<b>\1<\/b>/smg;
+#output result
+print $input;
 exit 0;
